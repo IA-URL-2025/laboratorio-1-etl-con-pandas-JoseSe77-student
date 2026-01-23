484d24b8dc50b9e5dd66b880d3ccb00e8576ad54
JoseSe77-student <jpsosae@correo.url.edu.gt>
2026-01-22 20:38:53 -0600

    Tested solution

--- src/etl.py
+++ src/etl.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
 def run_etl():
-    """
-    Implementa el proceso ETL.
-    No cambies el nombre de esta función.
-    """
-    # TODO: implementar
-    pass
+    df = pd.read_csv('data/citas_clinica.csv')
 
+    df["fecha_cita"] = pd.to_datetime(df["fecha_cita"], errors="coerce")
+    df = df.dropna(subset=["fecha_cita"])
+
+    df = df[ (df["estado"] == "CONFIRMADA") & (df["costo"] > 0) ]
+
+    df["paciente"] = df["paciente"].str.title()
+    df["especialidad"] = df["especialidad"].str.upper()
+
+    df["telefono"] = df["telefono"].fillna("NO REGISTRA")
+
+    df.to_csv("data/output.csv", index=False)
 
 if __name__ == "__main__":
     run_etl()
